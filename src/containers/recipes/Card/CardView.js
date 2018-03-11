7c458901b6fcc232bd7285962854ecb89b8eea4f
Vansh Khanna <vkhan4@sapient.com>
2018-03-11 15:58:45 +0530

    added editable name feature

--- src/containers/recipes/Card/CardView.js
+++ src/containers/recipes/Card/CardView.js
@@ -1,10 +1,10 @@
 /**
- * Recipe View Screen
- *  - The individual recipe screen
- *
- * React Native Starter App
- * https://github.com/mcnamee/react-native-starter-app
- */
+* Recipe View Screen
+*  - The individual recipe screen
+*
+* React Native Starter App
+* https://github.com/mcnamee/react-native-starter-app
+*/
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
@@ -12,7 +12,8 @@ import {
   View,
   StyleSheet,
   TouchableOpacity,
-  Button
+  Button,
+  TextInput
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Sizes from '@theme/sizes';
@@ -31,32 +32,33 @@ let totalMaps = 0;
 /* Styles ==================================================================== */
 const style = StyleSheet.create({
 
-mapCard: {
-  paddingLeft: 15,
-  paddingRight: 15,
-  paddingTop: 20,
-  paddingBottom: 20,
-  borderColor: 'gray',
-  shadowOpacity: 0.4,
-  elevation: 3,
-  shadowRadius: 2,
-  marginLeft: 5,
-  marginRight: 5,
-  marginTop: 15,
-  backgroundColor: 'white',
-  shadowOffset:{  width: 2,  height: 2 },
-  shadowColor: 'black',
-}
+  mapCard: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 20,
+    paddingBottom: 20,
+    borderColor: 'gray',
+    shadowOpacity: 0.4,
+    elevation: 3,
+    shadowRadius: 2,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 15,
+    backgroundColor: 'white',
+    shadowOffset:{  width: 2,  height: 2 },
+    shadowColor: 'black',
+  }
 });
 
 /* Component ==================================================================== */
 class Map extends Component {
   static componentName = 'Map';
   constructor() {
-  super();
-  this.hasData = false;
-  this.activeCard = {id: 0};
-};
+    super();
+    this.hasData = false;
+    this.activeCard = {id: 0};
+    this.modalVisible = false;
+  };
 
 
   static propTypes = {
@@ -68,68 +70,109 @@ class Map extends Component {
     return   fetch('http://ec2-18-216-151-224.us-east-2.compute.amazonaws.com:8000/getTrackerData', {
       method: 'POST',
       headers: {
-          Accept: 'application/json',
-          }
+        Accept: 'application/json',
+      }
     });
   };
+
+  setNickName = () => {
+    var nickname = '';
+    var card  = this.setNickName.card;
+    if (this.modalVisible) {
+      return (<View
+        style={[{opacity: 0.9}, {position: 'absolute', elevation: 4, top: ( Sizes.screen.width / 1.5 ), height: 150, width: Sizes.screen.width - 40, marginLeft: 20, backgroundColor: 'white', }]}
+        key = {'nicknameModal'}
+        >
+
+        <Text style={{fontSize: 15}}>  set nickname: </Text>
+        <TextInput
+        style={{paddingBottom: 10, paddingLeft: 10}}
+        onChangeText={(text) => { nickname = text }}
+
+        />
+
+        <View style={{flexDirection:'row', flexWrap:'wrap', marginTop: 40}}>
+        <View style={{width: '30%'}}>
+        <Button
+        onPress={() => { this.modalVisible = false; card.nickname = nickname; this.forceUpdate(); }}
+        title="ok"
+        color="black"
+        />
+        </View>
+        <View style={{width: '30%'}}></View>
+        <View style={{width: '40%'}}>
+        <Button
+        onPress={() => { this.modalVisible = false; this.forceUpdate(); }}
+        title="cancel"
+        color="black"
+        />
+        </View>
+        </View>
+
+        </View>
+      );
+    }
+  }
+
 
   toggleCardVisibility  = (cardID) => {
     try {
       if (this.activeCard.id === cardID) this.activeCard.id = 0;
       else this.activeCard.id = cardID;
-  } catch (err) {
-    Toast.show('unable to close this card');
-  }
+    } catch (err) {
+      Toast.show('unable to close this card');
+    }
     this.forceUpdate();
   };
+
   showCard(card, i) {
     var that = this;
     if (card.id === this.activeCard.id) {
-       return (
-      <View style={[style.mapCard, {flexDirection:'row', flexWrap:'wrap', marginTop: 0}]} key={this.state[this.mapKeys[i]].id}>
+      return (
+        <View style={[style.mapCard, {flexDirection:'row', flexWrap:'wrap', marginTop: 0}]} key={this.state[this.mapKeys[i]].id}>
         <View style={{flexDirection:'row', flexWrap:'wrap'}}>
-            <Icon  name="map-marker" size={25} />
-            <Text>&nbsp;&nbsp;&nbsp;&nbsp;</Text>
-            <Text style={{paddingTop: 5}}>Saraswati vidya mandir</Text>
-            <Text>{'\n'}{'\n'}</Text>
+        <Icon  name="map-marker" size={25} />
+        <Text>&nbsp;&nbsp;&nbsp;&nbsp;</Text>
+        <Text style={{paddingTop: 5}}>Saraswati vidya mandir</Text>
+        <Text>{'\n'}{'\n'}</Text>
         </View>
 
         <View style={{flexDirection:'row', flexWrap:'wrap'}}>
-            <Icon name="location-arrow" size={25} />
-            <Text>&nbsp;&nbsp;&nbsp;&nbsp;</Text>
-            <Text style={{paddingTop: 5}}>{this.state[this.mapKeys[i]].latitude + ' N,' + this.state[this.mapKeys[i]].longitude + ' E'}</Text>
-            <Text>{'\n'}{'\n'}{'\n'}{'\n'}{'\n'}</Text>
+        <Icon name="location-arrow" size={25} />
+        <Text>&nbsp;&nbsp;&nbsp;&nbsp;</Text>
+        <Text style={{paddingTop: 5}}>{this.state[this.mapKeys[i]].latitude + ' N,' + this.state[this.mapKeys[i]].longitude + ' E'}</Text>
+        <Text>{'\n'}{'\n'}{'\n'}{'\n'}{'\n'}</Text>
         </View>
 
 
         <View style={{flexDirection:'row', flexWrap:'wrap'}}>
-            <View style={{width: '50%'}}>
-              <Button
-                  onPress={() => Actions.geofensing({device: that.state[that.mapKeys[i]]})}
-                  title="GEO-FENSING"
-                  color="black"
-                  accessibilityLabel="Geo-Fence the tracker"
-              />
-            </View>
+        <View style={{width: '50%'}}>
+        <Button
+        onPress={() => Actions.geofensing({device: that.state[that.mapKeys[i]]})}
+        title="GEO-FENSING"
+        color="black"
+        accessibilityLabel="Geo-Fence the tracker"
+        />
+        </View>
 
-            <View style={{width: '30%'}}></View>
+        <View style={{width: '30%'}}></View>
 
-            <View style={{width: '20%'}}>
-              <Button
-                  onPress={()=>{}}
-                  title="EDIT"
-                  color="black"
-              />
-            </View>
+        <View style={{width: '20%'}}>
+        <Button
+        onPress={() => { this.modalVisible = true; this.setNickName.card = card; this.forceUpdate()} }
+        title="EDIT"
+        color="black"
+        />
+        </View>
 
         </View>
-     </View>
-    );
-  } else return (<Text></Text>)
-}
+        </View>
+      );
+    } else return (<Text></Text>)
+  }
   onPressLearnMore =  (i) => {
-      this.hasData = false;
-      this.forceUpdate();
+    this.hasData = false;
+    this.forceUpdate();
   }
   relocateToMarker() {
     this.forceUpdate();
@@ -145,67 +188,67 @@ class Map extends Component {
   }
 
   render = () => {
-  let toRender = [];
-  let that = this;
-  if (this.hasData) {
-
+    let toRender = [];
+    let that = this;
+    if (this.hasData) {
       for (let i=0; i<this.mapKeys.length; i++) {
 
         if (!this.state[this.mapKeys[i]].latitudeDelta) this.state[this.mapKeys[i]].latitudeDelta = 0.2;
         if (!this.state[this.mapKeys[i]].longitudeDelta) this.state[this.mapKeys[i]].longitudeDelta = 0.2;
-      toRender.push(
-<View key={this.state[this.mapKeys[i]].id}>
-    <TouchableOpacity activeOpacity={0.8} style={[style.mapCard, {flex: 1, flexDirection: 'row', justifyContent: 'space-between'}]} onPress= {() => this.toggleCardVisibility(this.state[this.mapKeys[i]].id)} key={this.state[this.mapKeys[i]].id}>
-      <View style={{flexDirection:'row', flexWrap:'wrap'}} >
-        <Icon name="warning" size={35}  />
-            <Text>&nbsp;&nbsp;&nbsp;&nbsp;</Text>
-              <Text style={{ flexDirection:'row', flexWrap:'wrap', fontSize: 20}}>{this.state[this.mapKeys[i]].id}({'Vansh'}) {'\n'}{'\n'}<Text>
-                 Last Update: {this.state[this.mapKeys[i]].datetime}</Text>
-                  <Text>{'\n'}{'\n'}</Text><Text>Battery Status: <Text>{this.state[this.mapKeys[i]].battery}%</Text></Text>
-              </Text>
-            <Text>&nbsp;&nbsp;&nbsp;&nbsp;</Text>
+        toRender.push(
+          <View key={this.state[this.mapKeys[i]].id}>
+          <TouchableOpacity activeOpacity={0.8} style={[style.mapCard, {flex: 1, flexDirection: 'row', justifyContent: 'space-between'}]} onPress= {() => this.toggleCardVisibility(this.state[this.mapKeys[i]].id)} key={this.state[this.mapKeys[i]].id}>
+          <View style={{flexDirection:'row', flexWrap:'wrap'}} >
+          <Icon name="warning" size={35}  />
+          <Text>&nbsp;&nbsp;&nbsp;&nbsp;</Text>
+          <Text style={{ flexDirection:'row', flexWrap:'wrap', fontSize: 20}}>{this.state[this.mapKeys[i]].id}({!this.state[this.mapKeys[i]].nickname === true ? this.state[this.mapKeys[i]].id : this.state[this.mapKeys[i]].nickname}) {'\n'}{'\n'}<Text>
+          Last Update: {this.state[this.mapKeys[i]].datetime}</Text>
+          <Text>{'\n'}{'\n'}</Text><Text>Battery Status: <Text>{this.state[this.mapKeys[i]].battery}%</Text></Text>
+          </Text>
+          <Text>&nbsp;&nbsp;&nbsp;&nbsp;</Text>
           <TouchableOpacity activeOpacity={0.2} onPress={() => Actions.trackerMap({device: that.state[that.mapKeys[i]]})} >
-              <Icon  name="map" size={35}  />
+          <Icon  name="map" size={35}  />
           </TouchableOpacity>
-    </View>
-  </TouchableOpacity>
-  <View>{this.showCard(this.state[this.mapKeys[i]], i)}</View>
-  </View>
-  );
-}
-  return (
-<View style={{backgroundColor: 'white'}}>
-  <View>
-    {toRender}
-  </View>
-</View>
-);
+          </View>
+          </TouchableOpacity>
+          <View>{this.showCard(this.state[this.mapKeys[i]], i)}</View>
+          </View>
+        );
+      }
+      toRender.push(this.setNickName());
+      return (
+        <View style={{backgroundColor: 'white'}}>
+        <View>
+        {toRender}
+        </View>
+        </View>
+      );
 
-} else {
-  let mapsDataPromise = this.getMapsData();
-  this.mapKeys = [];
-  let that = this;
-  let { title, onPress } = this.props;
-  let count  = this.mapKeys.length;
-  style.mapCard.height =  (100 / count).toString() + '%';
+    } else {
+      let mapsDataPromise = this.getMapsData();
+      this.mapKeys = [];
+      let that = this;
+      let { title, onPress } = this.props;
+      let count  = this.mapKeys.length;
+      style.mapCard.height =  (100 / count).toString() + '%';
 
 
-  mapsDataPromise.then ((res) => {
-    let mapData = JSON.parse(res._bodyInit);
-      that.mapKeys = Object.keys(mapData);
-      that.setState(mapData);
-      this.hasData = true;
-    }).catch((err) => {throw err;
-      Toast.show('failed getting tracker data', Toast.LONG);
-    });
-    return (
-      <View style={[style.container,  {
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            padding: 10
-  }]}>
-      <ActivityIndicator  style={{paddingTop: '50%'}} animating={!this.hasData} size="large" color="white" />
-      </View>
+      mapsDataPromise.then ((res) => {
+        let mapData = JSON.parse(res._bodyInit);
+        that.mapKeys = Object.keys(mapData);
+        that.setState(mapData);
+        this.hasData = true;
+      }).catch((err) => {throw err;
+        Toast.show('failed getting tracker data', Toast.LONG);
+      });
+      return (
+        <View style={[style.container,  {
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          padding: 10
+        }]}>
+        <ActivityIndicator  style={{paddingTop: '50%'}} animating={!this.hasData} size="large" color="white" />
+        </View>
       );
     }
   }
